da06aa5515332fdade181122b406f3918f56cbad
Monir <moniru005@gmail.com>
2023-11-29 18:49:56 +0600

    contact form connect with server

--- src/Pages/Contact/Contact.jsx
+++ src/Pages/Contact/Contact.jsx
@@ -1,6 +1,44 @@
 import { Helmet } from "react-helmet-async";
+import useAxiosPublic from "../../Hooks/useAxiosPublic";
+import Swal from "sweetalert2";
 
 const Contact = () => {
+
+    const axiosPublic = useAxiosPublic();
+
+    const handleContactSubmit = e =>{
+        e.preventDefault();
+        const form = e.target;
+        const firstName = form.firstName.value;
+        const lastName = form.lastName.value;
+        const email = form.email.value;
+        const phone = form.phone.value;
+        const message = form.message.value;
+
+        const contactInfo = {
+            firstName: firstName,
+            lastName: lastName,
+            email: email,
+            phone: phone,
+            message: message,
+        }
+
+        console.log(contactInfo);
+        axiosPublic.post('/contacts', contactInfo)
+        .then(res =>{
+            console.log(res.data);
+            if(res.data.insertedId){
+                Swal.fire({
+                    position: "center",
+                    icon: "success",
+                    title: `Thanks for for your feedback`,
+                    showConfirmButton: false,
+                    timer: 2500,
+                  });
+            }
+        })
+
+    }
   return (
     <div>
       <Helmet>
@@ -16,29 +54,35 @@ const Contact = () => {
                 Send us a <br /> message
               </h1>
             </div>
+            <form onSubmit={handleContactSubmit}>
             <div className="grid grid-cols-1 gap-5 md:grid-cols-2 mt-5">
               <input
                 className="w-full bg-gray-100 text-gray-900 mt-2 p-3 rounded-lg focus:outline-none focus:shadow-outline"
                 type="text"
                 placeholder="First Name*"
                 name="firstName"
+                required
               />
               <input
                 className="w-full bg-gray-100 text-gray-900 mt-2 p-3 rounded-lg focus:outline-none focus:shadow-outline"
                 type="text"
                 placeholder="Last Name*"
                 name="lastName"
+                required
               />
               <input
                 className="w-full bg-gray-100 text-gray-900 mt-2 p-3 rounded-lg focus:outline-none focus:shadow-outline"
                 type="email"
                 placeholder="Email*"
+                required
+                name="email"
               />
               <input
                 className="w-full bg-gray-100 text-gray-900 mt-2 p-3 rounded-lg focus:outline-none focus:shadow-outline"
                 type="number"
                 placeholder="Phone*"
-                name="email"
+                name="phone"
+                required
               />
             </div>
             <div className="my-4">
@@ -46,27 +90,23 @@ const Contact = () => {
                 placeholder="Message*"
                 name="message"
                 className="w-full h-32 bg-gray-100 text-gray-900 mt-2 p-3 rounded-lg focus:outline-none focus:shadow-outline"
+                required
               ></textarea>
             </div>
             <div className="my-2 w-1/2 lg:w-1/4">
-              <button
-                className="uppercase text-sm font-medium tracking-wide bg-blue-900 text-gray-100 p-3 rounded-lg w-full 
-                      focus:outline-none focus:shadow-outline"
-              >
-                Send Message
-              </button>
+              <input type="submit" value="Send Message"
+                className=" bg-blue-900 text-gray-100 py-3 px-4 rounded-lg cursor-pointer" />
             </div>
+            </form>
           </div>
 
-          <div className="w-full lg:-mt-96 lg:w-2/6 px-8 py-12 ml-auto bg-blue-900 rounded-2xl">
+          <div className="w-full lg:-mt-[450px] lg:w-2/6 px-8 py-12 ml-auto bg-blue-900 rounded-2xl">
             <div className="flex flex-col text-white">
               <h1 className="font-bold uppercase text-4xl my-4">
                 Drop in our office
               </h1>
               <p className="text-gray-400">
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam
-                tincidunt arcu diam, eu feugiat felis fermentum id. Curabitur
-                vitae nibh viverra, auctor turpis sed, scelerisque ex.
+              If you would like to drop in and speak with us about your case, please call our office at (212) 545-2220. We are located in the heart of Manhattan, near Grand Central Station.
               </p>
 
               <div className="flex my-4 w-2/3 lg:w-1/2">
@@ -76,40 +116,12 @@ const Contact = () => {
                 <div className="flex flex-col">
                   <h2 className="text-2xl">Main Office</h2>
                   <p className="text-gray-400">
-                    5555 Tailwind RD, Pleasant Grove, UT 73533
+                  11 East 44th Street Suite 819 New York, NY 10017
                   </p>
                 </div>
               </div>
 
-              <div className="flex my-4 w-2/3 lg:w-1/2">
-                <div className="flex flex-col">
-                  <i className="fas fa-phone-alt pt-2 pr-2" />
-                </div>
-                <div className="flex flex-col">
-                  <h2 className="text-2xl">Call Us</h2>
-                  <p className="text-gray-400">Tel: xxx-xxx-xxx</p>
-                  <p className="text-gray-400">Fax: xxx-xxx-xxx</p>
-                </div>
-              </div>
-
-              <div className="flex my-4 w-2/3 lg:w-1/2">
-                <a
-                  href="https://www.facebook.com/ENLIGHTENEERING/"
-                  target="_blank"
-                  rel="noreferrer"
-                  className="rounded-full bg-white h-8 w-8 inline-block mx-1 text-center pt-1"
-                >
-                  <i className="fab fa-facebook-f text-blue-900" />
-                </a>
-                <a
-                  href="https://www.linkedin.com/company/enlighteneering-inc-"
-                  target="_blank"
-                  rel="noreferrer"
-                  className="rounded-full bg-white h-8 w-8 inline-block mx-1 text-center pt-1"
-                >
-                  <i className="fab fa-linkedin-in text-blue-900" />
-                </a>
-              </div>
+              
             </div>
           </div>
         </div>
