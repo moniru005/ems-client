4c65329eabe03aa84f6a6c4243b76d639bd3a5e3
Monir <moniru005@gmail.com>
2024-01-07 12:22:31 +0600

    add new section- Who We Are

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -4,17 +4,17 @@ import ProductSuite from "./ProductSuite/ProductSuite";
 import Exclusive from "./Exclusive/Exclusive";
 import Testimonials from "./Testimonials/Testimonials";
 import CallToAction from "./CTA/CTA";
+import WhoWeAre from "../../Components/About/WhoWeAre";
 
 const Home = () => {
     return (
         <div>
             <Helmet> <title>Home | EMS</title> </Helmet>
             <Banner></Banner>
+            <WhoWeAre></WhoWeAre>
             <ProductSuite></ProductSuite>
             <Exclusive></Exclusive>
-            
             <Testimonials></Testimonials>
-           
             <CallToAction></CallToAction>
         </div>
     );
